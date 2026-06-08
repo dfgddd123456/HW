7b8b2aa915d35f0ef8f8c332d2602f528c64fb3f
dfgddd123456 <try_now123@mail.ru>
2026-06-09 00:46:44 +0300

    Delete projects_6/task_6_0 directory

--- projects_6/task_6_0/task_6-0_8.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-df = pd.read_csv('wild_boars.csv')
-
-std_tusk = df.groupby('gender')['tusk_length_cm'].std()
-mean_tusk = df.groupby('gender')['tusk_length_cm'].mean()
-
-with open('tusk_length.txt' , 'w', encoding='utf-8') as tusks:
-    tusks.write(f'Coefficient of task length variation: {(std_tusk / mean_tusk * 100)}')
